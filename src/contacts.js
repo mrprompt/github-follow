5ce5ebf6173e16ede3ba15ca7f0db474d198f86d
Thiago Paes <mrprompt@gmail.com>
2016-08-18 00:50:16 -0300

    ..

--- src/contacts.js
+++ src/contacts.js
@@ -10,36 +10,39 @@ function load(file) {
         return false;
     }
 
+    var token = process.env.GITHUB_CLIENT || '';
     var contactsList = JSON.parse(fs.readFileSync(file, 'utf8'));
-    var githubClient = github.client(process.env.GITHUB_CLIENT);
+    var githubClient = github.client(token);
     var me = githubClient.me();
     var search = githubClient.search();
 
     contactsList.connections.forEach(function(contact) {
-        if ("emailAddresses" in contact === false) {
-            return;
-        }
-
-        var email = contact.emailAddresses[0].value;
-
-        search.users({
-			q: `${email} in:email`,
-            sort: 'created',
-            order: 'asc'
-        }, function (err, data) {
-            if (err) {
-                console.error(err.body.message);
+        setTimeout(function() {
+            if ("emailAddresses" in contact === false) {
                 return;
             }
 
-            if (data.total_count > 0) {
-                var username = data.items[0].login;
+            var email = contact.emailAddresses[0].value;
 
-                me.follow(username);
+            search.users({
+                q: `${email} in:email`,
+                sort: 'created',
+                order: 'asc'
+            }, function (err, data) {
+                if (err) {
+                    console.error('ERROR: ' + err.body.message);
+                    return;
+                }
 
-                console.log(email + ' => @' + username);
-            }
-        })
+                if (data.total_count > 0) {
+                    var username = data.items[0].login;
+
+                    me.follow(username, function(err, result) {
+                        console.log(email + ' => @' + username);
+                    });
+                }
+            })
+        }, 10000);
     });
 }
 
